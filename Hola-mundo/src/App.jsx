b68334e664bda053ab57a1c34a4b0ccb09d240a1
garbanzo12 <122324891+garbanzo12@users.noreply.github.com>
2024-12-20 15:03:56 -0500

    Segunda sesion
    ...

--- Hola-mundo/src/App.jsx
+++ Hola-mundo/src/App.jsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+export function App() {
   return (
     <article>
       <header>
-        
+        <img src="https://unavatar.io/microlink/microlink.io" alt="avatar" />
+        <div>
+          <strong>Nombre del avatar</strong>
+          <span>@avatarname</span>
+        </div>
       </header>
+      <aside>
+        <button>
+          Seguir
+        </button>
+      </aside>
     </article>
   )
 }
